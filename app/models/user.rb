c21786503b5eb4287bc41f9d89b4925483e76723
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-08-03 12:30:23 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,13 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchases
   
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname, presence: true
+    validates :birthday, presence: true
+    validates :encrypted_password, format: {with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :last_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+  end
 end
