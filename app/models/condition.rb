7164b4f7b744a20ee814c4126e0179154cc9d285
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-08-05 21:21:03 +0900

    商品出品ページの表示

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,4 +1,4 @@
-class Genre < ActiveHash::Base
+class Condition < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '新品・未使用' },
@@ -12,5 +12,5 @@ class Genre < ActiveHash::Base
   include ActiveHash::Associations
   has_many :items
 
-  
+
   end
--- app/models/shipping_day.rb
+++ app/models/shipping_day.rb
@@ -1,4 +1,4 @@
-class Genre < ActiveHash::Base
+class ShippingDay < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '1~2日で発送' },
@@ -9,5 +9,5 @@ class Genre < ActiveHash::Base
   include ActiveHash::Associations
   has_many :items
 
-  
+
   end
