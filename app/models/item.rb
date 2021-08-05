75a292d861d8164b5703a7fdde3697f541ffe528
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-08-05 23:58:41 +0900

    with_optionでのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,8 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :prefecture
 
-  validates :product_name, :text, presence: true
-
-  validates :category_id, :condition_id,  :shipping_day_id, :shipping_fee_id,:prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  with_options presence: true do
+  validates :product_name,:text,:price
+  validates :category_id, :condition_id,  :shipping_day_id, :shipping_fee_id, :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  end
 end
