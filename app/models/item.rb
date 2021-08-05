2ead729062dc28b44551b140e6860f60f03a92ec
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-08-05 16:48:24 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_day
+  belongs_to :shipping_fee
+  belongs_to :prefecture
 end
