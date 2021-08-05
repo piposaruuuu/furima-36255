376fa5633a55968e1406a227d9c0a70cfcd0ceac
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-08-05 17:16:32 +0900

    バリデーションの設定（追加）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,15 @@
 class Item < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_day
   belongs_to :shipping_fee
   belongs_to :prefecture
+
+  validates :product_name, :text, presence: true
+
+  validates :category_id, :condition_id,  :shipping_day_id, :shipping_fee_id,:prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
