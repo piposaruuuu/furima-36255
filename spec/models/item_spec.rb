3e05bb185bb2f6cc2e88f3b870f754d578c0dc34
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-08-06 21:41:10 +0900

    テストコードの設定

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '入力に問題がなければ商品の出品ができる'  do
+      it '必須項目が全てあれば投稿できる' do
+      expect(@item).to be_valid 
+      end
+    
+      it '商品名が空だと出品できない' do
+        @item.product_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Product name can't be blank"
+      end
+      it '商品の説明が空だと出品できない' do
+        @item.text = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Text can't be blank"
+      end
+      it 'categoryが未選択だと出品できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category can't be blank"
+       end
+      it 'conditionが未選択だと出品できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Condition can't be blank"
+      end
+      it 'shipping_feeが未選択だと出品できない' do
+        @item.shipping_fee_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping fee can't be blank"
+      end
+      it 'prefectureが未選択だと出品できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Prefecture can't be blank"
+       end
+      it 'shipping_dayが未選択だと出品できない' do
+        @item.shipping_day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping day can't be blank"
+      end
+      it 'priceが空だと出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price can't be blank"
+       end
+    end
+  end
 end
