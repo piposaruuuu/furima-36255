c0862e7caf6eb544849ee49c2e41613be5f25fd1
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-08-07 13:19:28 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,7 @@ class Item < ApplicationRecord
   with_options presence: true do
   validates :product_name,:text,:image
   validates :category_id, :condition_id,  :shipping_day_id, :shipping_fee_id, :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."},numericality: {in: 300..9999999, message: "out of setting range"}
+  validates :price, numericality: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters"}
+  validates :price, inclusion: {in: 300..9999999, message: "out of setting range"}
   end
 end
