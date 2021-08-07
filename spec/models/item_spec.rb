c70377d0c5ff13c510d40c7d1d957115949ba9f6
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-08-07 13:19:13 +0900

    単体テストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,7 +9,13 @@ RSpec.describe Item, type: :model do
       it '必須項目が全てあれば投稿できる' do
       expect(@item).to be_valid 
       end
-    
+    end
+    context '入力に問題があれば商品の出品ができない'  do
+      it 'imageが空は出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Image can't be blank"
+       end
       it '商品名が空だと出品できない' do
         @item.product_name = ''
         @item.valid?
@@ -45,11 +51,51 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include "Shipping day can't be blank"
       end
+       it 'categoryが1では出品できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category can't be blank"
+       end
+      it 'conditionが1では出品できない' do
+        @item.condition_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Condition can't be blank"
+      end
+      it 'shipping_feeが1では出品できない' do
+        @item.shipping_fee_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping fee can't be blank"
+      end
+      it 'prefectureが1では出品できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Prefecture can't be blank"
+       end
+      it 'shipping_dayが1では出品できない' do
+        @item.shipping_day_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping day can't be blank"
+      end
       it 'priceが空だと出品できない' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include "Price can't be blank"
-       end
+        expect(@item.errors.full_messages).to include "Price can't be blank", "Price is invalid. Input half-width characters", "Price can't be blank", "Price out of setting range"
+      end
+      it 'priceが下限(300)未満では出品できない' do
+        @item.price = '100'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price out of setting range"
+      end
+      it 'priceが上限(9999999)を超えると出品できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price out of setting range"
+      end
+      it 'priceが全角数字では出品できない' do
+        @item.price = '３００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is invalid. Input half-width characters", "Price out of setting range"
+      end
     end
   end
 end
