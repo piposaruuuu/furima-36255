b13d3e56291d4031ab8dfbb185bcdddc891ef05a
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-08-08 19:53:10 +0900

    微修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,7 +42,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     unless current_user == @item.user
       redirect_to action: :index
     end
