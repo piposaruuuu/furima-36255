08bbbaec07569e584fb9568e8775bf8207147862
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-08-10 12:02:50 +0900

    モデルとテーブルの作成（purchase,delivery）

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -3,7 +3,7 @@ class PurchaseAddress
   attr_accessor :postal_code, :city, :block, :building_name, :phone_number, :prefecture_id, :user_id, :item_id
  
   with_options presence: true do
-    validates :city, :block, :building_name, :user_id, :item_id
+    validates :city, :block, :user_id, :item_id
     validates :prefecture_id,numericality: { other_than: 1, message: "can't be blank" }
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone_number, format: {with: /^(0{1}\d{9,10})$/, message: "is too short"}
