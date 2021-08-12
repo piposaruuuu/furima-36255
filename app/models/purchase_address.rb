1d42898994ed03e3b09ef1441ff9f9d8d31521ff
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-08-12 14:43:58 +0900

    モデル単体テストコードの作成

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -6,7 +6,7 @@ class PurchaseAddress
     validates :city, :block, :user_id, :item_id, :token
     validates :prefecture_id,numericality: { other_than: 1, message: "can't be blank" }
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :phone_number, format: {with: /\A(0{1}\d{9,10})\z/, message: "is too short"}
+    validates :phone_number, format: {with: /\A(0{1}\d{9,10})\z/, message: "is invalid"}
   end
 
   def save
---
+++ spec/factories/purchase_address.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :purchase_address do
+    postal_code        { '123-4567' }
+    prefecture_id      { 2 }
+    city               { '金沢市' }
+    block              {'鞍月1-1'}
+    building_name      { '東京ハイツ' }
+    phone_number       {'09012341234'}
+    token              {'tok_abcdefghijk00000000000000000'}
+  end
+end
